0852d8984840b48b3706b1f0dc78e0fc81a02d07
Tony Phondeth <phondeth@gmail.com>
2021-10-15 15:05:25 -0400

    comment-model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,6 +6,14 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({
+                _id: -1
+            })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -20,6 +28,11 @@ const pizzaController = {
         Pizza.findOne({
                 _id: params.id
             })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -55,7 +68,7 @@ const pizzaController = {
                 }
                 res.json(dbPizzaData);
             })
-            .catch(err => res.status(400).json(err));
+            .catch(err => res.json(err));
     },
 
     // delete pizza
@@ -65,16 +78,8 @@ const pizzaController = {
         Pizza.findOneAndDelete({
                 _id: params.id
             })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({
-                        message: 'No pizza found with this id!'
-                    });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
-            .catch(err => res.status(400).json(err));
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.json(err));
     }
 };
 
